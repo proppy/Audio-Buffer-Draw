dea8075effc76ca2790580ebaff23b1d8c7cfbc9
Johan Euphrosine <proppy@google.com>
2014-03-19 14:34:47 -0700

    refactor in a component, add base64 decoding

---
+++ js/drawbuffer.js
@@ -0,0 +1,17 @@
+function drawBuffer( width, height, context, buffer ) {
+    var data = buffer.getChannelData( 0 );
+    var step = Math.ceil( data.length / width );
+    var amp = height / 2;
+    for(var i=0; i < width; i++){
+        var min = 1.0;
+        var max = -1.0;
+        for (var j=0; j<step; j++) {
+            var datum = data[(i*step)+j];
+            if (datum < min)
+                min = datum;
+            if (datum > max)
+                max = datum;
+        }
+        context.fillRect(i,(1+min)*amp,1,Math.max(1,(max-min)*amp));
+    }
+}
